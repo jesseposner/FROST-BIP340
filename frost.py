e12ebc3d737c71e43e003b0591fe02a91e06eae3
Jesse Posner <jesse.posner@gmail.com>
2021-04-23 17:07:23 -0700

    Add secp256k1 class

--- frost.py
+++ frost.py
@@ -8,6 +8,16 @@
 """Python FROST adaptor signatures implementation."""
 
 class FROST:
+    class secp256k1:
+        P = 2**256 - 2**32 - 977
+        Q = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+        G_x = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
+        G_y = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+
+        @classmethod
+        def G(cls):
+            return FROST.Point(cls.G_x, cls.G_y)
+
     class Point:
         """Class representing an elliptic curve point."""
 
