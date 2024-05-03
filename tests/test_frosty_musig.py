c795daf51a7f57e5a4c4f56c68e5e2e2713339dc
Sanket Kanjalkar <sanketk@squareup.com>
2024-05-03 16:46:30 -0700

    Add sample test for frost and code for storing rij

---
+++ tests/test_frosty_musig.py
@@ -0,0 +1,116 @@
+import unittest
+
+import secrets
+from frost import Point, Participant, Aggregator, Q, G
+
+
+class Tests(unittest.TestCase):
+
+    def setUp(self):
+        self.p1 = Participant(index=1, threshold=2, participants=3)
+        self.p2 = Participant(index=2, threshold=2, participants=3)
+        self.p3 = Participant(index=3, threshold=2, participants=3)
+
+        p1 = self.p1
+        p2 = self.p2
+        p3 = self.p3
+
+        # Round 1.1, 1.2, 1.3, and 1.4
+        p1.init_keygen()
+        p2.init_keygen()
+        p3.init_keygen()
+
+        # Round 2.1
+        p1.generate_shares()
+        p2.generate_shares()
+        p3.generate_shares()
+
+        # Round 2.3
+        p1.aggregate_shares((p2.shares[p1.index - 1], p3.shares[p1.index - 1]))
+        p2.aggregate_shares((p1.shares[p2.index - 1], p3.shares[p2.index - 1]))
+        p3.aggregate_shares((p1.shares[p3.index - 1], p2.shares[p3.index - 1]))
+
+        # Round 2.4
+        p1.derive_public_key(
+            (p2.coefficient_commitments[0], p3.coefficient_commitments[0])
+        )
+        p2.derive_public_key(
+            (p1.coefficient_commitments[0], p3.coefficient_commitments[0])
+        )
+        p3.derive_public_key(
+            (p1.coefficient_commitments[0], p2.coefficient_commitments[0])
+        )
+
+        pk1 = p1.public_key
+        pk2 = p2.public_key
+        pk3 = p3.public_key
+
+        self.assertEqual(pk1, pk2)
+        self.assertEqual(pk2, pk3)
+    
+    def test_sign(self):
+        p1 = self.p1
+        p2 = self.p2
+        p3 = self.p3
+
+        # Round 1
+
+        Ri1_hashed = p1.sample_random_rij()
+        Ri2_hashed = p2.sample_random_rij()
+        Ri3_hashed = p3.sample_random_rij()
+        all_commitments = (Ri1_hashed, Ri2_hashed, Ri3_hashed)
+        p1.nonce_R_ij_commitments = all_commitments
+        p2.nonce_R_ij_commitments = all_commitments
+        p3.nonce_R_ij_commitments = all_commitments
+
+        # Round 2. Send nonce Rij to all parties
+        all_nonces = (p1.nonce_pair_r_ij[1], p2.nonce_pair_r_ij[1], p3.nonce_pair_r_ij[1])
+        p1.nonce_R_ij = all_nonces
+        p2.nonce_R_ij = all_nonces
+        p3.nonce_R_ij = all_nonces
+
+        p1.validate_other_party_nonce_commitments()
+        p2.validate_other_party_nonce_commitments()
+        p3.validate_other_party_nonce_commitments()
+
+        # Round 3.1
+        p1.compute_aggregate_nonce_R_i()
+        p2.compute_aggregate_nonce_R_i()
+        R_i = p3.compute_aggregate_nonce_R_i()
+
+        # # Obtain other parties aggregate nonce commitments
+        # p1.final_aggregate_R = R_i # THIS SHOULD BE FINAL VALUE FROM MUSIG
+        # p2.final_aggregate_R = None
+        # p3.final_aggregate_R = None
+
+        # # Round 3.2
+
+        # # Sign
+        # msg = b"fnord!"
+        # participant_indexes = (1, 2)
+        # agg = Aggregator(
+        #     pk,
+        #     msg,
+        #     (p1.nonce_commitment_pair, p2.nonce_commitment_pair),
+        #     participant_indexes,
+        # )
+        # message, nonce_commitment_pairs = agg.signing_inputs()
+
+        # s1 = p1.sign(message, nonce_commitment_pairs, participant_indexes)
+        # s2 = p2.sign(message, nonce_commitment_pairs, participant_indexes)
+
+        # # σ = (R, z)
+        # sig = agg.signature((s1, s2))
+        # sig_bytes = bytes.fromhex(sig)
+        # nonce_commitment = Point.xonly_deserialize(sig_bytes[0:32].hex())
+        # z = int.from_bytes(sig_bytes[32:64], "big")
+
+        # # verify
+        # # c = H_2(R, Y, m)
+        # challenge_hash = Aggregator.challenge_hash(nonce_commitment, pk, msg)
+        # # Negate Y if Y.y is odd
+        # if pk.y % 2 != 0:
+        #     pk = -pk
+
+        # # R ≟ g^z * Y^-c
+        # self.assertTrue(nonce_commitment == (z * G) + (Q - challenge_hash) * pk)
