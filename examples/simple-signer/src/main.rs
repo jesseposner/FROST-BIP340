eb9eea8922758fbc1fa71320314ba4e2b4b0510b
Jurvis Tan <jurvis@squareup.com>
2024-05-04 12:15:05 -0500

    Add CLI interface

--- examples/simple-signer/src/main.rs
+++ examples/simple-signer/src/main.rs
@@ -1,8 +1,34 @@
-// frosty-musings createaddress
-// frosty-musings signtranaction <unsigned_tx>
+use bitcoin::OutPoint;
+use clap::{Parser, Subcommand};
+
+#[derive(Clone, Parser)]
+#[clap()]
+pub struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Clone, Subcommand)]
+enum Commands {
+    CreateAddress {},
+    SignTransaction {
+        destination_address: String,
+        spending_prevout: OutPoint,
+    },
+}
+
 fn main() {
-    // 1. Create address from agg pubkey
-    // 2. Create tx that spends from output
-    // 3.
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::CreateAddress {} => {
+            println!("Creating address");
+        }
+        Commands::SignTransaction {
+            destination_address,
+            spending_prevout,
+        } => {
+            println!("Signing transaction");
+        }
+    }
 }
