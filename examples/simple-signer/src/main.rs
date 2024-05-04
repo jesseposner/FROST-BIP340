38c19a56ceb7576e3dca15f724beb5721a3642a1
Sanket Kanjalkar <sanketk@squareup.com>
2024-05-04 10:37:15 -0700

    Add sample transaction code

--- examples/simple-signer/src/main.rs
+++ examples/simple-signer/src/main.rs
@@ -1,8 +1,6 @@
-// frosty-musings createaddress
-// frosty-musings signtranaction <unsigned_tx>
-fn main() {
-    // 1. Create address from agg pubkey
-    // 2. Create tx that spends from output
-    // 3.
-    println!("Hello, world!");
-}
+use bitcoin;
+use clap::{Arg, Subcommand};
+
+mod transaction;
+
+fn main() {}
---
+++ examples/simple-signer/src/transaction.rs
@@ -0,0 +1,81 @@
+use std::process::Command;
+
+use bitcoin::hex::FromHex;
+use bitcoin::secp256k1::schnorr;
+use bitcoin::{hash_types, taproot, Network};
+/// Create a dummy transaction to spned from
+///
+use bitcoin::{
+    key::TweakedPublicKey, secp256k1, taproot::Signature, witness, Address, ScriptBuf, Transaction,
+    TxIn, Witness, XOnlyPublicKey,
+};
+
+pub fn create_address(agg_pk: bitcoin::PublicKey) -> Address {
+    let secp = secp256k1::Secp256k1::new();
+    let x_only_pk = TweakedPublicKey::dangerous_assume_tweaked(XOnlyPublicKey::from(agg_pk));
+    Address::p2tr_tweaked(x_only_pk, Network::Testnet)
+}
+
+pub fn create_transaction(prevout: bitcoin::OutPoint, addr: bitcoin::Address) -> Transaction {
+    let mut tx = Transaction {
+        version: bitcoin::transaction::Version(2),
+        lock_time: bitcoin::absolute::LockTime::from_height(2).unwrap(),
+        input: vec![TxIn {
+            previous_output: prevout,
+            script_sig: ScriptBuf::new(),
+            sequence: bitcoin::Sequence::MAX,
+            witness: Witness::new(),
+        }],
+        output: vec![bitcoin::TxOut {
+            value: bitcoin::Amount::from_sat(100_000),
+            script_pubkey: addr.script_pubkey(),
+        }],
+    };
+
+    // Call python code to get the signature
+    let output = Command::new("python3")
+                        .arg("/Users/sanketk/FROST-BIP340/examples/simple-signer/src/test.py")
+                        .arg("msg")
+                        .output()
+                        .expect("Failed to execute command");
+
+    let sig = if output.status.success() {
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        taproot::Signature {
+            signature: schnorr::Signature::from_slice(
+                &Vec::<u8>::from_hex(&stdout).unwrap()
+            ).unwrap(),
+            sighash_type: bitcoin::TapSighashType::Default,
+        }
+    } else {
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        panic!("Python script failed with error: {}", stderr);
+    };
+
+    tx.input[0].witness = Witness::p2tr_key_spend(&sig);
+    tx
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use bitcoin::hashes::Hash;
+
+    use super::create_transaction;
+
+
+    #[test]
+    fn test_python_stdout() {
+        let prevout = bitcoin::OutPoint {
+            txid: bitcoin::Txid::from_byte_array([0; 32]),
+            vout: 0,
+        };
+        let addr = bitcoin::Address::from_str("tb1pnzfn650wz5lptqttrhar64jycaf8tdy9gk69mud85dedww2lt3dsh73erq").unwrap();
+        let addr_checked = addr.assume_checked();
+        let tx = create_transaction(prevout, addr_checked);
+        let wit = tx.input[0].witness.to_vec();
+        assert!(wit.len() > 0);
+        assert!(wit[0].len() == 64);
+    }
+}
