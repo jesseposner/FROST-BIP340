c3210b3f7736821b086265b3d1fc618e5429fc76
Jurvis Tan <jurvis@squareup.com>
2024-05-04 11:59:36 -0500

    add examples

---
+++ examples/simple-signer/src/main.rs
@@ -0,0 +1,8 @@
+// frosty-musings createaddress
+// frosty-musings signtranaction <unsigned_tx>
+fn main() {
+    // 1. Create address from agg pubkey
+    // 2. Create tx that spends from output
+    // 3.
+    println!("Hello, world!");
+}
